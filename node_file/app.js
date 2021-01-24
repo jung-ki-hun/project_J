8e5e1573ce58ea60629ae64c05d37d93da3d2135
jungkihun <khkh0130@gmail.com>
2021-01-25 03:45:30 +0900

    jkh 1.0.0

---
+++ node_file/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var http = require('http'); // node 내장 모듈 불러옴 
+var static = require('serve-static');// 특정 폴더의 파일들을특정 패스로 접근할 수 있도록 만들어주는 외장 모듈
+var path = require('path');//경로
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var ip = `127.0.0.1`;///"203.241.228.134";//서버주소//당분간 로컬로 진행
+var app = express();
+app.set('port', process.env.PORT || 3000);//3000번 포트 개방
+app.disable(`x-powered-by`);
+http.createServer(app).listen(app.get('port'), ip, function () {
+
+	console.log('익스프레스로 웹 서버를 실행함 : ' + app.get('port'));
+	connectDB();
+
+}); 
