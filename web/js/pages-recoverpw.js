c57c89c8c266dc1cee4715d2a3bd230ef861f588
IJH <whdgus0515@naver.com>
2021-01-27 21:51:30 +0900

    ijh1.0.2

---
+++ web/js/pages-recoverpw.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const email = document.querySelector("#email"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        email: email.value
+    };
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
+}
