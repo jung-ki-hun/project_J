57824ae28956dab6937115d8d5815b7823f0a6ee
jungkihun <khkh0130@gmail.com>
2021-02-04 00:31:46 +0900

    jkh 1.0.9

--- node_file/app.js
+++ node_file/app.js
@@ -4,14 +4,13 @@ var path = require('path');//경로
 var static = require('serve-static');// 특정 폴더의 파일들을특정 패스로 접근할 수 있도록 만들어주는 외장 모듈
 var cookieParser = require('cookie-parser');
 var router = require(`./api/router.js`);
-//var ip = '127.0.0.1';///`180.83.98.144`;///"203.241.228.134";//서버주소//당분간 로컬로 진행
-var app = express();
+const app = express();
 const dataset ={
 	port : 3000,
-	host :'127.0.0.1'
+	host : '180.83.98.144' //'127.0.0.1'
 }
 
-app.use('/', router); //router 파일 읽어들이기
+app.use('/', router); //router 파일 읽어들이기 
 
 app.use('/web', static(path.join(__dirname, 'web')));//--dirmane : js 파일이 있는 폴더경로
 
