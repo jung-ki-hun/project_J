4cfc143c5601a02e3c10f4d356dfbbc041e1573e
Kim DongHun <112303dh@naver.com>
2021-03-16 21:37:02 +0900

    kdh 1.0.2

--- arduino_file/arduino_mega/arduino_mega.ino
+++ arduino_file/arduino_mega/arduino_mega.ino
@@ -1,40 +1,32 @@
-/*
-  Fade
-
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Fade
-*/
-
-int led = LED_BUILTIN;           // the PWM pin the LED is attached to
-int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
-
-// the setup routine runs once when you press reset:
+int A_motor_L = 32; //A_motor_L을 7번핀으로 설정합니다.
+int A_motor_R = 31; //A_motor_R을 8번핀으로 설정합니다.
+int B_motor_L = 34; //B_motor_L을 4번핀으로 설정합니다.
+int B_motor_R = 33; //B_motor_R을 7번핀으로 설정합니다.
+int A_motor_S = 30; //A_motor_S을 9번핀으로 설정합니다. (속도 제어)
+int B_motor_S = 35; //B_motor_S을 3번핀으로 설정합니다. (속도 제어)
 void setup() {
-  // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
+    pinMode(A_motor_L, OUTPUT); // Motor A 방향설정1
+    pinMode(A_motor_R, OUTPUT); // Motor A 방향설정2
+    pinMode(B_motor_L, OUTPUT); // Motor B 방향설정1
+    pinMode(B_motor_R, OUTPUT); // Motor B 방향설정2
 }
-
-// the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
-  analogWrite(led, brightness);
-
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
-
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
-  }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
+    /*모터A설정*/
+    digitalWrite(A_motor_L, HIGH); // Motor A 방향설정1
+    digitalWrite(A_motor_R, LOW); // Motor A 방향설정2
+    analogWrite(A_motor_S, 100); // Motor A 속도조절 (0~2B_motor_RB_motor_R)
+    /*모터B설정*/
+    digitalWrite(B_motor_L, LOW); // Motor B 방향설정1
+    digitalWrite(B_motor_R, HIGH); // Motor B 방향설정2
+    analogWrite(B_motor_S, 50); // Motor B 속도조절 (0~2B_motor_RB_motor_R)
+    delay(1000); // 3초 유지
+    /*모터A설정*/
+    digitalWrite(A_motor_L, LOW); // Motor A 방향설정1
+    digitalWrite(A_motor_R, HIGH); // Motor A 방향설정2
+    analogWrite(A_motor_S, 200); // Motor A 속도조절 (0~2B_motor_RB_motor_R)
+    /*모터B설정*/
+    digitalWrite(B_motor_L, HIGH); // Motor B 방향설정1
+    digitalWrite(B_motor_R, LOW); // Motor B 방향설정2
+    analogWrite(B_motor_S, 150); // Motor B 속도조절 (0~2B_motor_RB_motor_R)
+    delay(1000); // 3초 유지
 }
