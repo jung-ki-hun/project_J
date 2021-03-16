1624c758d0b1f86d4fdf8fba947a7d33f1092e0c
JH <whdgus0515@naver.com>
2021-03-17 04:38:44 +0900

    jkh_main 1.3.25

--- node_file/web/js/pages-login-name.js
+++ node_file/web/js/pages-login-name.js
@@ -8,11 +8,11 @@ var Succesful_login = () => {
         success: function (data, textStatus, jqXHR) {
             var json_o = JSON.parse(data);
             let json_d = json_o.query;
-            
+
             if (json_o.state == 1) {
                 let name = json_d;
                 console.log(json_d);
-                let str = `<form method="GET" action="/login">${name}님 환영합니다!<a href ="#">logout</a> </form>`;
+                let str = `<form method="GET" action="/login">${name}님 환영합니다!<&nbsp><&nbsp><&nbsp><&nbsp><a href ="#">logout</a> </form>`;
                 benner.innerHTML = str;
             } else {
                 let str = `<a href="pages-login.html">Login</a>`
@@ -20,15 +20,15 @@ var Succesful_login = () => {
             }
 
         },//login__main_benner
-        error : function(a, b, c){
+        error: function (a, b, c) {
             //통신 실패시 발생하는 함수(콜백)
             //alert(a + b + c);
             let str = `<a href="pages-login.html">Login</a>`
-                benner.innerHTML = str;
+            benner.innerHTML = str;
         }
     })
 }
 $(document).ready(() => {
     Succesful_login();
-    
+
 })
