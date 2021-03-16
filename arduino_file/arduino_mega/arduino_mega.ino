a36ed387f4e6f6c926c2d0ff7e0877b6246a1f9d
Kim DongHun <112303dh@naver.com>
2021-03-17 02:26:36 +0900

    kdh 1.0.2

--- arduino_file/arduino_mega/arduino_mega.ino
+++ arduino_file/arduino_mega/arduino_mega.ino
@@ -41,68 +41,91 @@ int B_motor_L = 32; //B_motor_L을 32번핀으로 설정합니다.
 int B_motor_R = 33; //B_motor_R을 33번핀으로 설정합니다.
 int B_motor_S = 2;  //B_motor_S을 2번핀으로 설정합니다. (속도 제어)
 
-// 2_RoadSensor
-
+// 2_LineSensor
+int Line_Sensor = 0;
 // 3_BlockSensor
+
+// 4_DEBUG_SERIAL
+
 void _1_Stop(void)
 {
-    digitalWrite(A_motor_L, HIGH); 
-    digitalWrite(A_motor_R, LOW);  
-    analogWrite(A_motor_S, 0);     
-    digitalWrite(B_motor_L, HIGH); 
-    digitalWrite(B_motor_R, LOW);  
-    analogWrite(B_motor_S, 0);     
+    digitalWrite(A_motor_L, HIGH);
+    digitalWrite(A_motor_R, LOW);
+    analogWrite(A_motor_S, 0);
+    digitalWrite(B_motor_L, HIGH);
+    digitalWrite(B_motor_R, LOW);
+    analogWrite(B_motor_S, 0);
 }
 void _1_Go(int speed)
 {
-    digitalWrite(A_motor_L, HIGH); 
-    digitalWrite(A_motor_R, LOW);  
-    analogWrite(A_motor_S, speed); 
-    digitalWrite(B_motor_L, HIGH); 
-    digitalWrite(B_motor_R, LOW);  
-    analogWrite(B_motor_S, speed); 
+    digitalWrite(A_motor_L, HIGH);
+    digitalWrite(A_motor_R, LOW);
+    analogWrite(A_motor_S, speed);
+    digitalWrite(B_motor_L, HIGH);
+    digitalWrite(B_motor_R, LOW);
+    analogWrite(B_motor_S, speed);
 }
 void _1_Back(int speed)
 {
-    digitalWrite(A_motor_L, LOW);  
-    digitalWrite(A_motor_R, HIGH); 
-    analogWrite(A_motor_S, speed); 
-    digitalWrite(B_motor_L, LOW);  
-    digitalWrite(B_motor_R, HIGH); 
-    analogWrite(B_motor_S, speed); 
+    digitalWrite(A_motor_L, LOW);
+    digitalWrite(A_motor_R, HIGH);
+    analogWrite(A_motor_S, speed);
+    digitalWrite(B_motor_L, LOW);
+    digitalWrite(B_motor_R, HIGH);
+    analogWrite(B_motor_S, speed);
 }
 void _1_Left(int speed)
 {
-    digitalWrite(A_motor_L, HIGH); 
-    digitalWrite(A_motor_R, LOW);  
-    analogWrite(A_motor_S, speed); 
-    digitalWrite(B_motor_L, LOW);  
-    digitalWrite(B_motor_R, HIGH); 
-    analogWrite(B_motor_S, speed); 
+    digitalWrite(A_motor_L, HIGH);
+    digitalWrite(A_motor_R, LOW);
+    analogWrite(A_motor_S, speed);
+    digitalWrite(B_motor_L, LOW);
+    digitalWrite(B_motor_R, HIGH);
+    analogWrite(B_motor_S, speed);
 }
 void _1_Right(int speed)
 {
-    digitalWrite(A_motor_L, LOW);  
-    digitalWrite(A_motor_R, HIGH); 
-    analogWrite(A_motor_S, speed); 
-    digitalWrite(B_motor_L, HIGH); 
-    digitalWrite(B_motor_R, LOW);  
-    analogWrite(B_motor_S, speed); 
+    digitalWrite(A_motor_L, LOW);
+    digitalWrite(A_motor_R, HIGH);
+    analogWrite(A_motor_S, speed);
+    digitalWrite(B_motor_L, HIGH);
+    digitalWrite(B_motor_R, LOW);
+    analogWrite(B_motor_S, speed);
+}
+void _2_Line(void)
+{
+}
+
+String _4_readSerial(void)
+{
+    String str = "";
+    char ch;
+    while (Serial.available() > 0)
+    {
+        ch = Serial.read();
+        str.concat(ch);
+        delay(10);
+    }
+    return str;
 }
 void setup()
 {
     // 0_Car
 
     // 1_Motor
-    pinMode(A_motor_L, OUTPUT); 
-    pinMode(A_motor_R, OUTPUT); 
-    pinMode(B_motor_L, OUTPUT); 
-    pinMode(B_motor_R, OUTPUT); 
+    pinMode(A_motor_L, OUTPUT);
+    pinMode(A_motor_R, OUTPUT);
+    pinMode(B_motor_L, OUTPUT);
+    pinMode(B_motor_R, OUTPUT);
     pinMode(LED_BUILTIN, OUTPUT);
 
     // 2_RoadSensor
 
     // 3_BlockSensor
+
+    // 4_DEBUG_SERIAL
+    Serial.begin(9600);
+
 }
 
 void loop()
@@ -116,11 +139,11 @@ void loop()
         _1_Stop();
         break;
     case 1:
-        //모터A설정
         _1_Go(0);
         break;
     case 2:
-        _1_Back(255);
+
+        break;
     default:
         break;
     }
